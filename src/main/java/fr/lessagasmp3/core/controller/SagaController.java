26190ed8324ac061b7ecd308dba3946c2f5f4a53
Thomah <5090230+Thomah@users.noreply.github.com>
2020-09-04 21:14:51 +0200

    Add basic web security for CORS filter and get all categories API

--- src/main/java/fr/lessagasmp3/core/controller/SagaController.java
+++ src/main/java/fr/lessagasmp3/core/controller/SagaController.java
@@ -1,6 +1,7 @@
 package fr.lessagasmp3.core.controller;
 
 import fr.lessagasmp3.core.entity.Saga;
+import fr.lessagasmp3.core.model.SagaModel;
 import fr.lessagasmp3.core.repository.SagaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,8 +19,11 @@ public class SagaController {
     private SagaRepository sagaRepository;
 
     @RequestMapping(value = "/api/sagas", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Saga> getAllSagas() {
-        return sagaRepository.findAll();
+    public List<SagaModel> getAll() {
+        List<Saga> entities = sagaRepository.findAll();
+        List<SagaModel> models = new ArrayList<>();
+        entities.forEach(entity -> models.add(SagaModel.fromEntity(entity)));
+        return models;
     }
 
 }
