eccf96c9fdc82893e6ea0873cbe3fa5b3139c1e8
Thomah <5090230+Thomah@users.noreply.github.com>
2020-09-14 17:11:23 +0200

    Add queries for saga querying

--- src/main/java/fr/lessagasmp3/core/controller/SagaController.java
+++ src/main/java/fr/lessagasmp3/core/controller/SagaController.java
@@ -5,31 +5,37 @@ import fr.lessagasmp3.core.model.SagaModel;
 import fr.lessagasmp3.core.repository.SagaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @RestController
+@RequestMapping("/api")
 public class SagaController {
 
     @Autowired
     private SagaRepository sagaRepository;
 
-    @RequestMapping(value = "/api/saga", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<SagaModel> getAll() {
-        List<Saga> entities = sagaRepository.findAll();
-        List<SagaModel> models = new ArrayList<>();
+    @RequestMapping(value = "/saga", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Set<SagaModel> getAll() {
+        Set<Saga> entities = sagaRepository.findAllByOrderByTitleAsc();
+        Set<SagaModel> models = new LinkedHashSet<>();
         entities.forEach(entity -> models.add(SagaModel.fromEntity(entity)));
         return models;
     }
 
-    @RequestMapping(value = "/api/saga/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/saga/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public SagaModel getById(@PathVariable Long id) {
         return SagaModel.fromEntity(sagaRepository.findById(id).orElse(null));
+    }
+
+    @RequestMapping(value = "/saga", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, params = {"search"})
+    public Set<SagaModel> search(@RequestParam("search") String search) {
+        Set<SagaModel> models = new LinkedHashSet<>();
+        Set<Saga> entities = sagaRepository.findAllByTitleContainsIgnoreCaseOrderByTitleAsc(search);
+        entities.forEach(entity -> models.add(SagaModel.fromEntity(entity)));
+        return models;
     }
 
 }
