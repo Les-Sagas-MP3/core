7762ee3c06615dcd7fdd74ca25225cd343185179
Thomah <5090230+Thomah@users.noreply.github.com>
2021-10-30 11:19:37 +0200

    Fix file upload

--- src/main/java/fr/lessagasmp3/core/controller/FileController.java
+++ src/main/java/fr/lessagasmp3/core/controller/FileController.java
@@ -7,8 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,11 +28,12 @@ public class FileController {
     @Value("${fr.lessagasmp3.core.storage}")
     private String storageFolder;
 
-    @RequestMapping(value = "/file/upload", method = RequestMethod.POST, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public String upload(@RequestParam("file") MultipartFile multipartFile,
-                                             @RequestParam("directory") String directoryPath,
-                                             @RequestParam("name") String name,
-                                             @RequestParam(name = "saveInDb", value = "false", required = false) Boolean saveInDb) {
+    @PostMapping(value = "/file/upload", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public String upload(@RequestParam(name = "file") MultipartFile multipartFile,
+                             @RequestParam(name = "directory") String directory,
+                             @RequestParam(name = "name") String name,
+                             @RequestParam(name = "saveInDb", defaultValue = "false", required = false) Boolean saveInDb) {
+
         if (multipartFile == null) {
             throw new RuntimeException("You must select the a file for uploading");
         }
@@ -54,9 +55,9 @@ public class FileController {
             finalFileName = name + extension;
             log.debug("saved filename: " + finalFileName);
 
-            prepareDirectories(directoryPath);
+            prepareDirectories(directory);
 
-            fullPath = storageFolder + File.separator + directoryPath + File.separator + finalFileName;
+            fullPath = storageFolder + File.separator + directory + File.separator + finalFileName;
             File file = new File(fullPath);
             try (OutputStream os = new FileOutputStream(file)) {
                 os.write(multipartFile.getBytes());
@@ -66,7 +67,7 @@ public class FileController {
             if (saveInDb) {
                 entity.setContent(Files.readString(Paths.get(file.getAbsolutePath())));
             }
-            entity.setDirectory(directoryPath);
+            entity.setDirectory(directory);
             entity.setName(name);
             entity.setPath(file.getAbsolutePath());
             fileRepository.save(entity);
@@ -82,7 +83,7 @@ public class FileController {
         File directory = new File(storageFolder);
         if (!directory.exists()) {
             log.info("Creating path {}", directory.getPath());
-            if(!directory.mkdirs()) {
+            if (!directory.mkdirs()) {
                 log.error("The path {} cannot be created", directory.getPath());
             }
         }
@@ -95,7 +96,7 @@ public class FileController {
             log.debug("Prepare directory {}", directory.getAbsolutePath());
             if (!directory.exists()) {
                 log.info("Creating path {}", directory.getPath());
-                if(!directory.mkdirs()) {
+                if (!directory.mkdirs()) {
                     log.error("Cannot create directory {}", directory.getAbsolutePath());
                 }
             }
