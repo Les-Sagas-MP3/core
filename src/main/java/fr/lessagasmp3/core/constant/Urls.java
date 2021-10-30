f09c68c35182cef2372a101e3f2a75b0a68b48a6
Thomah <5090230+Thomah@users.noreply.github.com>
2021-10-30 11:57:38 +0200

    Setup Cloudinary

---
+++ src/main/java/fr/lessagasmp3/core/constant/Urls.java
@@ -0,0 +1,30 @@
+package fr.lessagasmp3.core.constant;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Urls {
+
+    public static final String PROTOCOL = "protocol";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String HOST = "host";
+
+    public static Map<String, String> splitUrl(String url) {
+        String pattern = "^(\\w+?://)(\\w+?):(.+?)@(.+?)$";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(url);
+        Map<String, String> split = new HashMap<>();
+        if (m.find( )) {
+            split.put(PROTOCOL, m.group(0));
+            split.put(USERNAME, m.group(1));
+            split.put(PASSWORD, m.group(2));
+            split.put(HOST, m.group(3));
+        } else {
+            throw new IllegalArgumentException("No match");
+        }
+        return split;
+    }
+}
